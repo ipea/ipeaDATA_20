fa822f84d456cefb571f692f18d884953a1fca95
Guilherme Vergara <guifayvergara@hotmail.com>
2017-04-05 18:10:33 -0300

    arrumando arquivos para o padrao

--- prototipo/src/index.js
+++ prototipo/src/index.js
@@ -1,23 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import {Grid, Col} from 'react-bootstrap'
 
 import Navbar from './components/Navbar'
-// import Tabs from './components/Tabs'
-// import Menu from './components/Menu'
- import Tabs from './components/Tabs'
 
-
-
-// <Grid>
-//     <Row className="show-grid">
-//       <Col xs={12} md={8}><code>&lt;{'Col xs={12} md={8}'} /&gt;</code></Col>
-//       <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
-//     </Row>
-// </Grid> 
-
-render(<Tabs/>, document.querySelector('#main2'));
 render(<Navbar/>, document.querySelector('#main'));
-// render(<Tabs/>, document.querySelector('#main2'));
-// render(<Menu/>, document.querySelector('#main3'));
-
