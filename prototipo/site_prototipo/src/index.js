704a8408405f248d6f0d41be8ffa3db6b3c04215
BrunoRangel30 <rangel.bruno28@gmail.com>
2017-04-06 17:42:51 -0300

    adição da pasta aulas e prototipo

---
+++ prototipo/site_prototipo/src/index.js
@@ -0,0 +1,14 @@
+// let's go!
+
+import React from 'react';
+import { render} from 'react-dom';
+import {BrowserRouter, Match, Miss} from 'react-router';
+//import './css/style.css';
+import StorePicker from './components/StorePicker';
+import App from './components/App';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+
+    render (<App/>, document.querySelector('#main'));
+       
+       
--- prototipo/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-
-import Navbar from './components/Navbar'
-
-render(<Navbar/>, document.querySelector('#main'));
